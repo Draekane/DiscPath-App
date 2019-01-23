41fc8e281d8e169fd53093c996845e8f8a37b6cf
Michael Palmer <michael_s_palmer@homedepot.com>
2019-01-23 09:28:41 -0800

    Updates to the Python Scripts
    And thus, updates to the data

--- src/InboundDisc_FlightData.py
+++ src/InboundDisc_FlightData.py
@@ -1,4 +1,3 @@
-import bs4
 import json
 import urllib.request, urllib.parse
 import requests
@@ -25,7 +24,9 @@ print("Company,Name,Type,Range,HST,LSF,PDGA Approved,Matrix")
 disc_companies = set([disc["value"].split("|")[0] for disc in discTable])
 
 for company in disc_companies:
-	if company.strip() != "":
+	if company.strip() != "" and company.strip() != "Plackard1" and company.strip() != "Plackard2" and company.strip() != "Plackard3" and company.strip() != "Plackard4" and company.strip() != "sortabletable":
+	
+		
 		company_discs = [disc for disc in discTable if disc["value"].split("|")[0] == company]
 		linkregex = re.compile('http').search
 		link_check = [link for disc in company_discs for link in (linkregex(disc["value"]),) if link]
@@ -33,51 +34,34 @@ for company in disc_companies:
 			print("Company: " + company.strip())
 			print("Disc Count:" + str(len(company_discs)))
 			print("------------------------------------------------------------------------------------------------")
-			pathlib.Path('./src/data/Discs').mkdir(parents=True, exist_ok=True)
 			shortenedName = company.strip().replace(" ","")
-			filename = "./src/data/Discs/" + shortenedName + "_collection.json"
-			out_handle = open(filename,'w')
-			# print("{")
-			# print("    \"company\": \"" + company + "\",")
-			# print("    \"companyId\": ,")
-			# print("    \"discs\": [")
-			out_handle.write("{\n\t\"company\": \"" + company + "\",\n\t\"companyId\": \"" + str(hashlib.md5(str.encode(shortenedName)).hexdigest()) +"\",\n\t\"discs\": [\n")
+			currentCompany = { "company": company.strip(), "companyId": str(hashlib.md5(str.encode(shortenedName)).hexdigest()), "discs": [] }
+			pathlib.Path('./src/data/Discs').mkdir(parents=True, exist_ok=True)
+			
 			disc_count = 0
 			for disc in company_discs:
 				disc_count += 1
 				disc_data_arr = disc["value"].split("|")
-				if disc_count == 1:
-					# print("    {")
-					out_handle.write("\t\t{\n")
-				if disc_count > 1:
-					# print("    } , {")
-					out_handle.write("\t\t} , {\n")
+				currentDisc = {}	
 				if len(disc_data_arr) >= 2:
-					# print("        \"discId\": " + str(disc_count) + ",")
-					out_handle.write("\t\t\t\"discId\": \"" + str(hashlib.md5(str.encode(disc_data_arr[1])).hexdigest()) + "\",\n")
-					# print("        \"name\": \"" + disc_data_arr[1] + "\",")
-					out_handle.write("\t\t\t\"name\": \"" + disc_data_arr[1] + "\",\n")
+					currentDisc["discId"] = str(hashlib.md5(str.encode(disc_data_arr[1])).hexdigest())
+					currentDisc["name"] = disc_data_arr[1]
 				if len(disc_data_arr) >= 3:
-					# print("        \"type\": \"" + disc_data_arr[2][:1] + "\",")
-					out_handle.write("\t\t\t\"type\": \"" + disc_data_arr[2][:1] + "\",\n")
+					currentDisc["type"] = disc_data_arr[2][:1]
 				if len(disc_data_arr) >= 4:
-					# print("        \"range\": " + disc_data_arr[3] + ",")
-					out_handle.write("\t\t\t\"range\": " + disc_data_arr[3] + ",\n")
+					currentDisc["range"] = int(disc_data_arr[3])
 				if len(disc_data_arr) >= 5:
-					# print("        \"hst\": " + disc_data_arr[4].replace("%","") + ",")
-					out_handle.write("\t\t\t\"hst\": " + disc_data_arr[4].replace("%","") + ",\n")
+					currentDisc["hst"] = int(disc_data_arr[4].replace("%",""))
 				if len(disc_data_arr) >= 6:
-					# print("        \"lsf\": " + disc_data_arr[5].replace("%","") + ",")
-					out_handle.write("\t\t\t\"lsf\": " + disc_data_arr[5].replace("%","") + ",\n")
+					currentDisc["lsf"] = int(disc_data_arr[5].replace("%",""))
 				if len(disc_data_arr) >= 8:
-					# print("        \"pdga\": \"" + disc_data_arr[7] + "\",")
-					out_handle.write("\t\t\t\"pdga\": \"" + disc_data_arr[7] + "\",\n")
+					currentDisc["pdga"] = disc_data_arr[7]
 				if len(disc_data_arr) >= 10:
-					# print("        \"matrix\": \"" + disc_data_arr[9] + ",")
-					# print("        \"matrix_x\": \"" + disc_data_arr[9].split("-")[1] + ",")
-					# print("        \"matrix_y\": \"" + disc_data_arr[9].split("-")[0] + ",")
-					out_handle.write("\t\t\t\"matrix_x\": " + disc_data_arr[9].split("-")[1] + ",\n\t\t\t\"matrix_y\": " + disc_data_arr[9].split("-")[1] + "\n")
-			# print("    }]")
-			# print("}")
-			out_handle.write("\t}]\n}\n")
+					matrixSplit = disc_data_arr[9].split("-")
+					currentDisc["matrix_x"] = int(matrixSplit[1])
+					currentDisc["matrix_y"] = int(matrixSplit[0])
+					currentCompany["discs"].append(currentDisc)
+			filename = "./src/data/Discs/" + shortenedName + "_collection.json"
+			out_handle = open(filename,'w')
+			out_handle.write(json.dumps(currentCompany, indent=4))
 			out_handle.close()
